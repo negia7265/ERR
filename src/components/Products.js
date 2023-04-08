490e245e5ce07f564a07e991da68b1918313bb8c
Ayush Negi <96509253+negia7265@users.noreply.github.com>
2023-04-08 17:39:38 +0530

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "./data";
 import ProductItems from "./ProductItems";
-
+import axios from "axios";
 const Container = styled.div`
   padding: 20px;
   display: flex;
@@ -11,12 +11,60 @@ const Container = styled.div`
 `;
 
 export default function Products({ cat, filter, sort }) {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  function isFiltered(item) {
+    if (
+      filter.color &&
+      filter.size &&
+      item.color.includes(filter.color) &&
+      item.size.includes(filter.size)
+    )
+      return true;
+    else if (item.color && item.color.includes(filter.color)) return true;
+    else if (item.size && item.size.includes(filter.size)) return true;
+    else return false;
+  }
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products/all?category=${cat}`
+            : "http://localhost:5000/api/allproducts"
+        );
+        setProducts(res.data);
+        setFilteredProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, [cat]);
+  // useEffect(() => {
+  //   cat &&
+  //     setFilteredProducts(
+  //       products.filter(
+  //         (item) =>
+  //           // Object.entries(filter).every(([key, value]) =>
+  //           //   item[key].includes(value)
+  //           // )
+  //           item.color[0] === filter.color
+  //       )
+  //     );
+  // }, [products, cat, filter]);
+  useEffect(() => {
+    async function filtered() {
+      cat && setFilteredProducts(products.filter((item) => isFiltered(item)));
+    }
+    filtered();
+  }, [products, cat, filter]);
   return (
     <div>
       <Container>
-        {popularProducts.map((item) => (
-          <ProductItems item={item} />
-        ))}
+        {filteredProducts.length === 0
+          ? products.map((item) => <ProductItems item={item} />)
+          : filteredProducts.map((item) => <ProductItems item={item} />)}
       </Container>
     </div>
   );
